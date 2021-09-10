1a404762f84740ec9d368ad63ee76bcc29f4c92d
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2021-09-10 07:34:48 +0200

    Create Mouse trail.js

---
+++ Eloquent JavaScript/15. Handling Events/Mouse trail.js
@@ -0,0 +1,8 @@
+window.addEventListener('mousemove', ev => {
+    let div = document.createElement('div');
+    div.className = 'dot';
+    div.style.left = ev.pageX - 4 + 'px';
+    div.style.top = ev.pageY - 4 + 'px';
+    document.body.appendChild(div);
+    setTimeout(() => div.remove(), 200);
+});
