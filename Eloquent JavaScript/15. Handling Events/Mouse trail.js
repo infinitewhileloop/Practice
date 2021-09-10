b4e53584b14084b9e2832c8ddb58e393353f00a9
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2021-09-10 07:36:22 +0200

    Delete Eloquent JavaScript directory

--- Eloquent JavaScript/15. Handling Events/Mouse trail.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('mousemove', ev => {
-    let div = document.createElement('div');
-    div.className = 'dot';
-    div.style.left = ev.pageX - 4 + 'px';
-    div.style.top = ev.pageY - 4 + 'px';
-    document.body.appendChild(div);
-    setTimeout(() => div.remove(), 200);
-});
