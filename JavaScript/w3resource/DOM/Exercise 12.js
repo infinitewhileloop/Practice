a0f6d54415e2554ce76218592f4eca012ed2c1e3
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2021-09-10 07:49:59 +0200

    Update Exercise 12.js

--- JavaScript/w3resource/DOM/Exercise 12.js
+++ JavaScript/w3resource/DOM/Exercise 12.js
@@ -4,3 +4,10 @@ function highlight() {
 
  	strongEls.forEach(item => item.style.background = 'gold');
  } 
+
+function return_normal() {
+
+ 	let strongEls = document.querySelectorAll('strong');
+
+ 	strongEls.forEach(item => item.style.background = '');
+} 
