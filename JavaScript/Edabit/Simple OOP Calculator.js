79a0e1710f3bd530b59dc6b7d2c7de1e6aef7f73
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2021-09-13 13:46:22 +0200

    Create Simple OOP Calculator.js

---
+++ JavaScript/Edabit/Simple OOP Calculator.js
@@ -0,0 +1,17 @@
+// https://edabit.com/challenge/yxKoCKemzacK6PECM
+
+class Calculator {
+    constructor() { }
+    add(x, y) {
+        return x + y;
+    }
+    subtract(x, y) {
+        return x - y;
+    }
+    multiply(x, y) {
+        return x * y;
+    }
+    divide(x, y) {
+        return x / y;
+    }
+}
