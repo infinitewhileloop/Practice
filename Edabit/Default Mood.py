520212c68aeeddf117fcce863d1ef858976d92eb
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2021-09-18 20:34:10 +0200

    Delete Default Mood.py

--- Edabit/Default Mood.py
+++
@@ -1,8 +0,0 @@
-#https://edabit.com/challenge/tgEWKRQD8hu5dD3pX
-
-def mood_today(*args):
-    if len(args):
-        word = args[0]
-    else:
-        word = 'neutral'
-    return 'Today, I am feeling ' + word
