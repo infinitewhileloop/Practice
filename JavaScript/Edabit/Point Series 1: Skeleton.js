2c50dd3035a20d1f39a9afbb994f7112c5a7a363
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2021-11-06 13:42:29 +0100

    Create Point Series 1: Skeleton.js

---
+++ JavaScript/Edabit/Point Series 1: Skeleton.js
@@ -0,0 +1,11 @@
+// https://edabit.com/challenge/HwFtgwoW2qbQnoD6s
+
+class Point {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	toString() {
+		return `[x=${this.x}, y=${this.y}]`;
+	}
+}
