fee256f60dbd7663506314a88b85b4c3ec5784f5
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2021-11-10 22:18:47 +0100

    Create High Scores.js

---
+++ JavaScript/Exercism/High Scores.js
@@ -0,0 +1,11 @@
+// https://exercism.org/tracks/javascript/exercises/high-scores
+
+export class HighScores {
+    constructor(input) {
+        this._scores = input;
+    }
+    get scores() { return this._scores }
+    get latest() { return this._scores[this._scores.length - 1] }
+    get personalBest() { return Math.max(...this._scores) }
+    get personalTopThree() { return [...this._scores].sort((a, b) => b - a).slice(0, 3) }
+}
