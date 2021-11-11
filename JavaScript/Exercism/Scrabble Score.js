a7701c948baadb3522c8893d4eceea50a4aaad26
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2021-11-11 14:25:06 +0100

    Create Scrabble Score.js

---
+++ JavaScript/Exercism/Scrabble Score.js
@@ -0,0 +1,42 @@
+// https://exercism.org/tracks/javascript/exercises/scrabble-score
+
+const score = word => {
+
+    return [...word.toUpperCase()].map(char => {
+        switch (char) {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            case 'L':
+            case 'N':
+            case 'R':
+            case 'S':
+            case 'T':
+                return 1;
+            case 'D':
+            case 'G':
+                return 2;
+            case 'B':
+            case 'C':
+            case 'M':
+            case 'P':
+                return 3;
+            case 'F':
+            case 'H':
+            case 'V':
+            case 'W':
+            case 'Y':
+                return 4;
+            case 'K':
+                return 5;
+            case 'J':
+            case 'X':
+                return 8;
+            case 'Q':
+            case 'Z':
+                return 10;
+        }
+    }).reduce((acc, cur) => acc + cur, 0)
+};
