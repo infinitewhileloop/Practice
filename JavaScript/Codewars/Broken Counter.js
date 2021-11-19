e0493d1979e492ef4f414c74ccc14db0d374f590
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2021-11-20 00:14:37 +0100

    Create Broken Counter.js

---
+++ JavaScript/Codewars/Broken Counter.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/526471539d52735c620000c6/solutions/
+
+class Counter {
+
+    constructor() {
+        this.value = 0;
+    }
+    increase() {
+        this.value++;
+    }
+    getValue() {
+        return this.value;
+    }
+    reset() {
+        this.value = 0;
+    }
+}
