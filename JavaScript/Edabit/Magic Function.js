1125dd7fea77922643cb0016d4a2f1fb32b07453
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2021-11-27 12:58:05 +0100

    Create Magic Function.js

---
+++ JavaScript/Edabit/Magic Function.js
@@ -0,0 +1,16 @@
+// https://edabit.com/challenge/nhW7dXvLWrQoepyFs
+
+let magic = {
+    replace: function(str, oldChar, newChar) {
+        return str.replace(oldChar, newChar);
+    },
+    length: function(str) {
+        return str.length;
+    },
+    trim: function(str) {
+        return str.trim();
+    },
+    slice: function(arr, start, end) {
+        return arr.slice(start, end);
+    }
+}
