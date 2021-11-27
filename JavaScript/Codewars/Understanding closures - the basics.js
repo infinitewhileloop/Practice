b1ddbf9b969468cefb5108cca54857c72868b698
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2021-11-27 17:21:48 +0100

    Create Understanding closures - the basics.js

---
+++ JavaScript/Codewars/Understanding closures - the basics.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/56b71b1dbd06e6d88100092a/
+
+function buildFun(n) {
+
+    let res = [];
+
+    for (let i = 0; i < n; i++) {
+        res.push(function() {
+            return i;
+        });
+    };
+
+    return res;
+}
