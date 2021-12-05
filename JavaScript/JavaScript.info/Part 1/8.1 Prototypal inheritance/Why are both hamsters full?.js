58645a097ddc8889b5dfe9110fd1c8f9cfa0f342
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2021-12-05 17:07:25 +0100

    Create Why are both hamsters full?.js

---
+++ JavaScript/JavaScript.info/Part 1/8.1 Prototypal inheritance/Why are both hamsters full?.js
@@ -0,0 +1,21 @@
+// https://javascript.info/prototype-inheritance#why-are-both-hamsters-full
+
+let hamster = {
+    stomach: [],
+
+    eat(food) {
+        if (!this.hasOwnProperty('stomach')) {
+            this.stomach = [food]
+        } else {
+            this.stomach.push(food);
+        }
+    }
+}
+
+let speedy = {
+    __proto__: hamster,
+};
+
+let lazy = {
+    __proto__: hamster,
+};
