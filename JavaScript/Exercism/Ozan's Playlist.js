29d440fa40ac5dd2ace7b5f1e5d62bdf0570f20c
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2021-12-09 15:21:31 +0100

    Create Ozan's Playlist.js

---
+++ JavaScript/Exercism/Ozan's Playlist.js
@@ -0,0 +1,63 @@
+// https://exercism.org/tracks/javascript/exercises/ozans-playlist
+
+/**
+ * Removes duplicate tracks from a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} new playlist with unique entries
+ */
+export function removeDuplicates(arr) {
+    let set = new Set(arr);
+    return [...set];
+}
+
+/**
+ * Checks whether a playlist includes a track.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {boolean} whether the track is in the playlist
+ */
+export function hasTrack(playlist, track) {
+  return new Set(playlist).has(track);
+}
+
+/**
+ * Adds a track to a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function addTrack(playlist, track) {
+  let set = new Set(playlist);
+  set.add(track);
+  return Array.from(set);
+}
+
+/**
+ * Deletes a track from a playlist.
+ *
+ * @param {string[]} playlist
+ * @param {string} track
+ * @returns {string[]} new playlist
+ */
+export function deleteTrack(playlist, track) {
+  let set = new Set(playlist);
+  set.delete(track);
+  let arr = [];
+  set.forEach(item => arr.push(item));
+  return arr;
+}
+
+/**
+ * Lists the unique artists in a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} list of artists
+ */
+export function listArtists(playlist) {
+  let artists = playlist.map(item => item.split(' - ')[1]);
+  let uniq = new Set(artists);
+  return [...uniq];
+}
