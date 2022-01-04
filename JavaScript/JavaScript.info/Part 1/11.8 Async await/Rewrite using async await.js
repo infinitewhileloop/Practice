81aa38fa34044954ecf10fdf179e42512da03690
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2022-01-04 15:21:19 +0100

    Create Rewrite using async await.js

---
+++ JavaScript/JavaScript.info/Part 1/11.8 Async await/Rewrite using async await.js
@@ -0,0 +1,17 @@
+// https://javascript.info/async-await#rewrite-using-async-await
+
+async function loadJson(url) {
+
+    let response = await fetch(url);
+
+    if (response.status === 200) {
+        let json = await response.json();
+        return json;
+
+    } else {
+        throw new Error(response.status);
+    }
+}
+
+loadJson('no-such-user.json')
+    .catch(alert);
