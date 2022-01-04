ef32bbf9bb31db16de08007e8fb33ed4de137ca4
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2022-01-04 16:01:28 +0100

    Create Rewrite "rethrow" with async await.js

---
+++ JavaScript/JavaScript.info/Part 1/11.8 Async await/Rewrite "rethrow" with async await.js
@@ -0,0 +1,44 @@
+// https://javascript.info/async-await#rewrite-rethrow-with-async-await
+
+class HttpError extends Error {
+    constructor(response) {
+        super(`${response.status} for ${response.url}`);
+        this.name = 'HttpError';
+        this.response = response;
+    }
+}
+
+async function loadJson(url) {
+
+    let response = await fetch(url);
+
+    if (response.status === 200) {
+        let json = await response.json();
+        return json;
+    } else {
+        throw new HttpError(response);
+    }
+}
+
+async function demoGithubUser() {
+
+    while (true) {
+
+        let name = prompt('Enter a name?', 'iliakan');
+
+        try {
+            let user = await loadJson(`https://api.github.com/users/${name}`);
+            alert(`Full name: ${user.name}`);
+            return user;
+
+        } catch (err) {
+            if (err instanceof HttpError && err.response.status === 404) {
+                alert('No such user, please reenter.');
+            } else {
+                throw err;
+            }
+        }
+    }
+}
+
+demoGithubUser();
