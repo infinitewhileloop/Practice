0697865913bd0c4624e84e3157e99b6b004f84aa
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2022-01-05 15:59:36 +0100

    Create Resistor Color.js

---
+++ JavaScript/Exercism/Resistor Color.js
@@ -0,0 +1,6 @@
+// https://exercism.org/tracks/javascript/exercises/resistor-color
+
+export const colorCode = color => COLORS.indexOf(color);
+
+export const COLORS = ['black','brown','red','orange','yellow','green',
+                       'blue','violet','grey','white'];
