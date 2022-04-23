cb84d99d0a5771ca4b18617544be4b2400ca7e02
infinitewhileloop <84473512+infinitewhileloop@users.noreply.github.com>
2022-04-23 10:32:22 +0200

    Create http-client.js

---
+++ JavaScript/NodeSchool/learnyounode/http-client.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const log = console.log;
+const http = require('http');
+
+const url = process.argv[2];
+
+http.get(url, (res) => {
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+        log(chunk);
+    });
+});
